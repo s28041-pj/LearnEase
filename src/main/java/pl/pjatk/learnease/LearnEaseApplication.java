ab753b789abcca64dc8580b63a4b9e4deade4464
miko <mik.gurgul@gmail.com>
2024-11-16 07:14:17 +0100

    initial commit

---
+++ src/main/java/pl/pjatk/learnease/LearnEaseApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.learnease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnEaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnEaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pjatk/learnease/LearnEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.learnease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnEaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
